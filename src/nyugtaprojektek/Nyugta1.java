a9a8dafe3a0967ead4b58abe10213f0aa75ad06d
konnan933 <konnan933@gmail.com>
2022-03-18 12:25:54 +0100

    Kiirások összeadások osztások metodusba helyezése

--- src/nyugtaprojektek/Nyugta1.java
+++ src/nyugtaprojektek/Nyugta1.java
@@ -3,30 +3,37 @@ package nyugtaprojektek;
 public class Nyugta1 {
 
     public static void main(String[] args) {
-        System.out.println("******************");
-        System.out.println("     Nyugta 1");
-        System.out.println("******************");
-        System.out.println("Tétel 1:    350 Ft");
-        System.out.println("Tétel 2:     90 Ft");
-        System.out.println("Tétel 3:    650 Ft");
-        System.out.println("==================");
-        System.out.println("Összesen:  1090 Ft");
-        System.out.println("------------------");
-        System.out.println("Kedvezmény: 109 Ft");
-        System.out.println("(10%)");
-        System.out.println("==================");
-        System.out.println("Fizetendő:  981 Ft");
-        System.out.println("------------------");
-        System.out.println("");
-        System.out.print("_______");
-        System.out.print("   ");
-        System.out.println("_______");
-        System.out.print(" Dátum");
-        System.out.print("   ");
-        System.out.println("   Név");
-        System.out.println("******************");
-        System.out.println("        CÉG");
-        System.out.println("******************");
+        kiiras("******************");
+        kiiras("     Nyugta 1");
+        kiiras("******************");
+        kiiras("Tétel 1:    350 Ft");
+        kiiras("Tétel 2:     90 Ft");
+        kiiras("Tétel 3:    650 Ft");
+        kiiras("==================");
+        kiiras("Összesen:  1090 Ft");
+        kiiras("------------------");
+        kiiras("Kedvezmény: 109 Ft");
+        kiiras("(10%)");
+        kiiras("==================");
+        kiiras("Fizetendő:  981 Ft");
+        kiiras("------------------");
+        kiiras("");
+        kiirassortoresnelkul("_______");
+        kiirassortoresnelkul("   ");
+        kiiras("_______");
+        kiirassortoresnelkul(" Dátum");
+        kiirassortoresnelkul("   ");
+        kiiras("   Név");
+        kiiras("******************");
+        kiiras("        CÉG");
+        kiiras("******************");
+    }
+
+    private static void kiiras(String szoveg) {
+        System.out.println(szoveg);
+    }
+    private static void kiirassortoresnelkul(String szoveg) {
+        System.out.print(szoveg);
     }
     
 }
--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -6,69 +6,84 @@ a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzegh
 */
 public class Nyugta3 {
 
-    public static void main(String[] args) {
+    
+    private static void kiirasln(String szoveg) {
+        System.out.println(szoveg);
+    }
+    private static void kiirast(String szoveg) {
+        System.out.print(szoveg);
+    }
+    private static void kiirasf(String szoveg, String szoveg2,int elem1,String elem2) {
+        System.out.printf(szoveg, szoveg2, elem1, elem2);
+    }
+    private static int osszeadas(int szam1, int szam2) {
+        
+        return szam1+szam2;
+    }
+    private static int osztas(int szam1, int szam2) {
+        
+        return szam1/szam2;
+    }
+    private static void csillagKiiras() {
         String csillagok = "********************";
-        System.out.println(csillagok);
-        //System.out.println("     Nyugta 3");
-        System.out.printf("%14s\n", "Nyugta 3");
-        System.out.println(csillagok);
-        
-        final String HUF = "Ft";
-        int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
-//        System.out.printf("Tétel 1:     %d %s\n", tetel1, huf);
-//        System.out.printf("Tétel 1:      %d %s\n", tetel2, huf);
-//        System.out.printf("Tétel 3:    %d %s\n", tetel3, huf);
-        System.out.printf("%10s: %5d %s\n", "Tétel 1", tetel1, HUF);
-        System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
-        System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
-
+        kiirasln(csillagok);
+    }
+    private static void duplaKiiras() {
         String duplaVonal = "====================";
-        System.out.println(duplaVonal);
-        
-        int osszesen = tetel1 + tetel2 + tetel3;
-        //System.out.printf("Összesen:   %d %s\n", osszesen, huf);
-        System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
-        
-        String szaggatottVonal = "--------------------";
-        System.out.println(szaggatottVonal);
-        
-        int szervizDijMertek = 10;
-        int szervizDij = osszesen / szervizDijMertek;
-        //System.out.printf("Kedvezmény:  %d %s\n", kedvezmeny, huf);
-        System.out.printf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
-        System.out.printf("(%d%%)\n", szervizDijMertek);
-        
-        System.out.println(duplaVonal);
-        
-        //int fizetendo = osszesen - szervizDij;
-        int fizetendo = osszesen + szervizDij;
-        //System.out.printf("Fizetendő:  %d %s\n", fizetendo, huf);
-        System.out.printf("%10s:  %d %s\n", "Fizetendő", fizetendo, HUF);
-        double euro = fizetendo / 350.0;
-        //HUF = "\u20ac";//EZ NEM JÓ!!!!
-        final String eur = "\u20ac";
-        //System.out.printf("            %f %s\n", euro, eur);
-        
-        /* 7.2 7 szélesen 2 tizedessel,a max: 1234.99
-        *  a %10s  egy "" -t ír ki, ezzel tolom beljebb
-        */
-        System.out.printf("%10s%7.2f %s\n","", euro, eur);//
-        
-        System.out.println(szaggatottVonal);
-        
-        System.out.println("");
+        kiirasln(duplaVonal);
+    }
+    private static void rovidVonalKiiras() {
         String rovidVonal = "_______";
-        System.out.print(rovidVonal);
-        String rovidVonalValaszto = "      ";
-        System.out.print(rovidVonalValaszto);
-        System.out.println(rovidVonal);
-        System.out.print(" Dátum");
-        System.out.print(rovidVonalValaszto);
-        System.out.println("   Név");
-        
-        System.out.println(csillagok);    
-        System.out.println("        CÉG");
-        System.out.println(csillagok);
+        kiirast(rovidVonal);
     }
 
+    
+    public static void main(String[] args) {
+        csillagKiiras();
+        System.out.printf("%14s\n", "Nyugta 3");
+        csillagKiiras();
+        final String HUF = "Ft";
+        int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
+        kiirasf("%10s: %5d %s\n", "Tétel 1", tetel1, HUF);
+        kiirasf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
+        kiirasf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
+
+        duplaKiiras();
+        
+        int osszesen = osszeadas(tetel1, tetel2);
+        int osszesen2 = osszeadas(osszesen,tetel3);
+        kiirasf("%10s: %5d %s\n", "Összesen", osszesen2, HUF);
+        String szaggatottVonal = "--------------------";
+        kiirasln(szaggatottVonal);
+        
+        int szervizDijMertek = 10;
+        int szervizDij = osztas(osszesen2, szervizDijMertek);
+      
+        kiirasf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
+        System.out.printf("(%d%%)\n", szervizDijMertek);
+        
+        duplaKiiras();
+        int fizetendo = osszeadas(osszesen2, szervizDij);
+        kiirasf("%10s:  %d %s\n", "Fizetendő", fizetendo, HUF);
+        double euro = fizetendo / 373.0;
+        final String eur = "\u20ac";
+        System.out.printf("%10s%7.2f %s\n","", euro, eur);//
+        
+        kiirasln(szaggatottVonal);
+        
+        kiirasln("");
+        rovidVonalKiiras();
+        String rovidVonalValaszto = "      ";
+        kiirast(rovidVonalValaszto);
+        rovidVonalKiiras();
+        kiirast(" Dátum");
+        kiirast(rovidVonalValaszto);
+        kiirasln("   Név");
+        
+        csillagKiiras();
+        kiirasln("        CÉG");
+        csillagKiiras();
+    }
+
+    
 }
